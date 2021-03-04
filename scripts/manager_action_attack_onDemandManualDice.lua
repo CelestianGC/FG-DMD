8d41293eeece762a99603e273614b1627a1ae1f1
Mike Wilson <uce.mikew@gmail.com>
2021-03-04 16:50:54 -0600

    2.7

--- scripts/manager_action_attack_onDemandManualDice.lua
+++ scripts/manager_action_attack_onDemandManualDice.lua
@@ -1,6 +1,7 @@
 function onInit()
   -- replace default roll with adnd_roll to allow
   -- control-dice click to prompt for manual roll
+  Debug.console("manager_action_attack_onDemandManualDice.lua","onInit","Initializing");
   ActionsManager.roll = adnd_roll;
   --
 end
@@ -9,7 +10,7 @@ end
 -- control-dice click to prompt for manual roll
 function adnd_roll(rSource, vTargets, rRoll, bMultiTarget)
 	if #(rRoll.aDice) > 0 then
-		if not rRoll.bTower and (OptionsManager.isOption("MANUALROLL", "on") or (Session.isHost and Input.isControlPressed())) then
+		if not rRoll.bTower and (OptionsManager.isOption("MANUALROLL", "on") or (Session.IsHost and Input.isControlPressed())) then
 			local wManualRoll = Interface.openWindow("manualrolls", "");
 			wManualRoll.addRoll(rRoll, rSource, vTargets);
 		else
