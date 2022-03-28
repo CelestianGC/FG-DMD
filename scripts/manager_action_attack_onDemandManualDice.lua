f67b45065d87b8e5e429e75ea46ea884cab51a02
Mike Wilson <uce.mikew@gmail.com>
2022-03-28 11:24:53 -0500

    3.0 changes because of CoreRPG changes
    3.0 changes because of CoreRPG changes

--- scripts/manager_action_attack_onDemandManualDice.lua
+++ scripts/manager_action_attack_onDemandManualDice.lua
@@ -9,10 +9,11 @@ end
 -- replace default roll with adnd_roll to allow
 -- control-dice click to prompt for manual roll
 function adnd_roll(rSource, vTargets, rRoll, bMultiTarget)
-	if #(rRoll.aDice) > 0 then
+	if ActionsManager.doesRollHaveDice(rRoll) then
+		DiceManager.onPreEncodeRoll(rRoll);
+	
 		if not rRoll.bTower and (OptionsManager.isOption("MANUALROLL", "on") or (Session.IsHost and Input.isControlPressed())) then
-			local wManualRoll = Interface.openWindow("manualrolls", "");
-			wManualRoll.addRoll(rRoll, rSource, vTargets);
+			ManualRollManager.addRoll(rRoll, rSource, vTargets);
 		else
 			local rThrow = ActionsManager.buildThrow(rSource, vTargets, rRoll, bMultiTarget);
 			Comm.throwDice(rThrow);
